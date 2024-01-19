14b74a998d1e3bf2a7d0574d2e83514a441d35f6
AnhKhoa112233 <126152545+AnhKhoa112233@users.noreply.github.com>
2024-01-19 18:59:31 +0700

    Lab1_Bai1
    Nhập xuất tên điểm trung bình

---
+++ Lab1/src/Lab1_Bai1_63134335.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+
+/**
+ * 
+ */
+public class Lab1_Bai1_63134335 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
