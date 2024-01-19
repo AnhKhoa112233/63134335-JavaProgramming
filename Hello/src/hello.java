b2efe712f2096befe7d43c70f75f53d20dec6724
AnhKhoa112233 <126152545+AnhKhoa112233@users.noreply.github.com>
2024-01-19 13:55:26 +0700

    CodeHello
    Bai code dau tien

---
+++ Hello/src/hello.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+
+/**
+ * 
+ */
+public class hello {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Hello,I'm Khoa");
+	}
+
+}
