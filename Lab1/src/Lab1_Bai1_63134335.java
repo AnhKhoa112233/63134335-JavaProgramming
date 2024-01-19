5176787f17eb7d98fe64d694500521dae4ed878b
AnhKhoa112233 <126152545+AnhKhoa112233@users.noreply.github.com>
2024-01-19 19:06:06 +0700

    Lab1_Bai1
    Nhập xuất dữ liệu

--- Lab1/src/Lab1_Bai1_63134335.java
+++ Lab1/src/Lab1_Bai1_63134335.java
@@ -1,18 +1,25 @@
-/**
- * 
- */
+import java.util.Scanner;
 
-/**
- * 
- */
 public class Lab1_Bai1_63134335 {
 
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		  Scanner scanner = new Scanner(System.in);
 
-	}
+	        // Nhập tên 
+	        System.out.print("Họ và tên: ");
+	        String hoTen = scanner.nextLine();
+
+	        // Nhập điểm TB
+	        System.out.print("Điểm TB : ");
+	        double diemTB = scanner.nextDouble();
+
+	        // Đóng Scanner sau khi sử dụng
+	        scanner.close();
+
+	        // Hiển thị thông tin sinh viên
+	        System.out.printf("%s %.2f diem",hoTen,diemTB);
+		}
+	
 
 }
